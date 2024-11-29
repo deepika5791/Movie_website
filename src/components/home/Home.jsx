5d217d60e9feba6eb5fdad80a1146068d8a9a5b8
Your Name <you@example.com>
2024-11-29 19:24:56 +0530

    some code chnages

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,58 +1,65 @@
-import React,{useEffect,useState} from 'react'
-import "./Home.css"
+import React, { useEffect, useState } from "react";
+import "./Home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import { NavLink } from "react-router-dom";
-import MovieLists from '../movieList/MovieLists';
-
-
+import MovieLists from "../movieList/MovieLists";
 
 const Home = () => {
- 
-    const [ popularMovies, setPopularMovies ] = useState([])
-
-    useEffect(() => {
-    fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
-    .then(res => res.json())
-    .then(data => setPopularMovies(data.results))
-    }, [])
-
-    return (
-    <div>
-    <div className="poster">
-    <Carousel
-    showThumbs={false}
-    autoPlay={true}
-    transitionTime={3}
-    infiniteLoop={true}
-    showStatus={false}
-    >
-    {
-    popularMovies.map(movie => (
-    <NavLink style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
-    <div className="posterImage">
-    <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
-    </div>
-    <div className="posterImage__overlay">
-    <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
-    <div className="posterImage__runtime">
-    {movie ? movie.release_date : ""}
-    <span className="posterImage__rating">
-    {movie ? movie.vote_average :""}
-    <i className="fas fa-star" />{" "}
-    </span>
-    </div>
-    <div className="posterImage__description">{movie ? movie.overview : ""}</div>
-    </div>
-    </NavLink>
-    ))
-    }
-    </Carousel>
-    <MovieLists />
-    </div>
-
-    </div>
+  const [popularMovies, setPopularMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=40483c9f3c0f0a703a690b4f57c64855"
     )
-}
+      .then((res) => res.json())
+      .then((data) => setPopularMovies(data.results));
+      
+  }, []);
+  return (
+    <div>
+      <div className="poster">
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          transitionTime={3}
+          infiniteLoop={true}
+          showStatus={false}
+        >
+          {popularMovies.map((movie) => (
+            <NavLink
+              style={{ textDecoration: "none", color: "white" }}
+              to={`/movie/${movie.id}`}
+            >
+              <div className="posterImage">
+              <img
+                  src={`https://image.tmdb.org/t/p/original${
+                    movie && movie.backdrop_path
+                  }`}
+                />
+              </div>
+              <div className="posterImage__overlay">
+                <div className="posterImage__title">
+                  {movie ? movie.original_title : ""} 
+                
+                </div>
+                <div className="posterImage__runtime">
+                  {movie ? movie.release_date : ""}
+                  <span className="posterImage__rating">
+                    {movie ? movie.vote_average : ""}
+                    <i className="fas fa-star" />
+                  </span>
+                </div>
+                <div className="posterImage__description">
+                  {movie ? movie.overview : ""}
+                </div>
+              </div>
+            </NavLink>
+          ))}
+        </Carousel>
+        <MovieLists />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/movieList/MovieLists.jsx
+++ src/components/movieList/MovieLists.jsx
@@ -1,40 +1,42 @@
-import React,{useEffect , useState} from 'react'
-import "./MovieLists.css"
-import { useParams } from "react-router-dom"
-import Carts from '../cart/Carts'
+import React, { useEffect, useState } from "react";
+import "./MovieLists.css";
+import { useParams } from "react-router-dom";
+import Carts from "../cart/Carts";
 
 const MovieLists = () => {
-    const [movieList, setMovieList] = useState([])
-    const {type} = useParams()
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
 
-    useEffect(() => {
-    getData()
-    }, [])
+  useEffect(() => {
+    getData();
+  }, [type]);
 
-    useEffect(() => {
-    getData()
-    }, [type])
-
-    const getData = () => {
-    fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-    .then(res => res.json())
-    .then(data => setMovieList(data.results))
-    }
-
-    return (
-    <div>
-    <div className="movie__list">
-    <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
-    <div className="list__cards">
-    {
-    movieList.map(movie => (
-    <Carts movie={movie} />
-    ))
-    }
-    </div>
-    </div>
-    </div>
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+       type ? type : "popular"
+      }?api_key=40483c9f3c0f0a703a690b4f57c64855&language=en-US`
+     
     )
-}
+      .then((res) => res.json())
+      .then((data) => setMovieList(data.results));
+  };
 
-export default MovieLists
+  return (
+    <div>
+      <div className="movie__list">
+        <h2 className="list__title">
+          {(type ? type : "POPULAR").toUpperCase()}
+        </h2>
+        <div className="list__cards">
+          {movieList.map((movie) => (
+            <Carts movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieLists;
+
